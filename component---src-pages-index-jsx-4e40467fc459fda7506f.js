2e3e002231e1079957213498c2279753d541f37a
artsbeacon <artsbeacon@users.noreply.github.com>
2021-07-12 15:44:57 +0000

    deployed via Gatsby Publish Action 🎩 for 98c1af3af95c1d5a9c2f10dc3beee8441f248c78

---
+++ component---src-pages-index-jsx-4e40467fc459fda7506f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{Dtc0:function(t,n,e){"use strict";e.r(n);var o=e("dI71"),a=e("q1tI"),c=e("Kvkj"),p=e("qKvR"),r=function(t){function n(){return t.apply(this,arguments)||this}return Object(o.a)(n,t),n.prototype.render=function(){var t=this.props.data.homepage;return Object(p.d)(c.b,null,Object(p.d)(c.a,null),Object(p.d)(c.e,{sections:t.data.page_sections}))},n}(a.Component);n.default=r}}]);
+//# sourceMappingURL=component---src-pages-index-jsx-4e40467fc459fda7506f.js.map
